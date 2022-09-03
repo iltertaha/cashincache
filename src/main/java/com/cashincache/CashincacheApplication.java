643e60fd33fcae687b24c54936607071539b3c54
iltertaha <ilteraktolga@gmail.com>
2022-09-03 21:22:09 +0300

    Add Spring Boot project

---
+++ src/main/java/com/cashincache/CashincacheApplication.java
@@ -0,0 +1,13 @@
+package com.cashincache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashincacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashincacheApplication.class, args);
+	}
+
+}
